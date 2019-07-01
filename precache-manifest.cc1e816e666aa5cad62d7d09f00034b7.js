e83ffe85500a9131d757176421675ff302a568a1
IuliiaSam <jukraine@gmail.com>
2019-07-01 23:31:27 +0300

    Updates

---
+++ precache-manifest.cc1e816e666aa5cad62d7d09f00034b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea524838a9995e66cbbe",
+    "url": "/bulls-and-cows-game/static/js/runtime~main.d4b6d4b5.js"
+  },
+  {
+    "revision": "5f230de0833bc27df7ee",
+    "url": "/bulls-and-cows-game/static/js/main.31442efd.chunk.js"
+  },
+  {
+    "revision": "afe6b5b1a8b757f19737",
+    "url": "/bulls-and-cows-game/static/js/2.9f9179e4.chunk.js"
+  },
+  {
+    "revision": "5f230de0833bc27df7ee",
+    "url": "/bulls-and-cows-game/static/css/main.7cf77b5a.chunk.css"
+  },
+  {
+    "revision": "c5f182e08b1df8a80494a4c4920d78a7",
+    "url": "/bulls-and-cows-game/index.html"
+  }
+];
